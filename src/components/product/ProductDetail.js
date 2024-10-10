a652c0fc4abd358052f17d45d197da36d80d6849
Nicole <janenieggcs@gmail.com>
2024-10-10 12:59:25 -0500

    Added styles to product details component

--- src/components/product/ProductDetail.js
+++ src/components/product/ProductDetail.js
@@ -1,25 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Accordion, Button } from 'react-bootstrap';
 import { LoadingComponent } from '../../components';
 
+
 const ProductDetail = ({ product }) => {
+
+    const [zoomPosition, setZoomPosition] = useState({ x: 0, y: 0 });
+    const [isZoomed, setIsZoomed] = useState(false);
+    const [buttonColor, setButtonColor] = useState('var(--color-dark-green)'); // Color inicial del botón (verde)
+
+
+    const handleMouseEnter = () => {
+        setIsZoomed(true);
+    };
+
+    const handleMouseMove = (e) => {
+        const { left, top, width, height } = e.target.getBoundingClientRect();
+        const x = ((e.clientX - left) / width) * 100;
+        const y = ((e.clientY - top) / height) * 100;
+        setZoomPosition({ x, y });
+    };
+
+    const handleMouseLeave = () => {
+        setIsZoomed(false);
+    };
+
+    const handleButtonClick = () => {
+        setButtonColor('var(--color-emerald-green)'); // Cambiar a un verde diferente al hacer clic
+        // Restaurar el color original después de 1 segundo (1000 ms)
+        setTimeout(() => {
+            setButtonColor('var(--color-dark-green)'); // Volver al color original
+        }, 1000);
+    };
+
     if (!product) return <LoadingComponent />
     return (
-        <div className="detail-product">
+        <div className="detail-product container">
             <div className='row'>
-                <div className='col'>
-                    <div className="detail-product__image">
+                <div className='col-md-6 d-flex align-items-center justify-content-center'>
+                    <div
+                        className="detail-product-image-wrapper"
+                        onMouseEnter={handleMouseEnter}
+                        onMouseMove={handleMouseMove}
+                        onMouseLeave={handleMouseLeave}
+                    >
                         {/* <img src={imageUrl} alt={name} /> */}
-                        <span>imagen</span>
+                        <img
+                            src='https://belcorpperu.vtexassets.com/arquivos/ids/272578-600-auto?v=638242833182930000&width=600&height=auto&aspect=true'
+                            alt={product.name}
+                            className={`detail-product-image ${isZoomed ? 'zoomed' : ''}`}
+                            style={{
+                                transformOrigin: `${zoomPosition.x}% ${zoomPosition.y}%`
+                            }}
+                        />
                     </div>
                 </div>
-                <div className='col'>
+                <div className='col-md-6'>
                     <div className="detail-product__info">
                         <h2>{product.name}</h2>
+                        <div className='divider-detail-product' />
                         <p className="detail-product__price">${product.price.toFixed(2)}</p>
-                        <p className="detail-product__description">{product.description}</p>
-                        <button className="detail-product__add-to-cart">Add to Cart</button>
+                        <Button
+                            className="detail-product__add-to-cart"
+                            style={{ backgroundColor: buttonColor, borderColor: buttonColor }} // Cambia el color del botón
+                            onClick={handleButtonClick} // Maneja el clic en el botón
+                        >
+                            Add to Cart
+                        </Button>
+
                     </div>
                 </div>
+            </div>
+
+            <div className='row'>
+                {/* Accordion for the description */}
+                <Accordion className='detail-product-accordion__description'>
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>
+                            <h3>Description</h3>
+                        </Accordion.Header>
+                        <Accordion.Body>
+                            <p className="detail-product__description">{product.description}</p>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
             </div>
         </div>
     )
